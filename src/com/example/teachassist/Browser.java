c42ca84230e664187b6e98053d9c4da1137d3069
Priyesh <priyesh.96@hotmail.com>
2014-01-18 12:01:25 -0500

    Add webview

--- src/com/example/teachassist/Browser.java
+++ src/com/example/teachassist/Browser.java
@@ -1,16 +1,33 @@
 package com.example.teachassist;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class Browser extends Activity{
 
+    private WebView web;
+
+	@SuppressLint("SetJavaScriptEnabled")
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.browser);
 		
+        web = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.getSettings().setUserAgentString("Mozilla/5.0 " +
+                "(Windows NT 6.2; " +
+                "WOW64) AppleWebKit/537.31 " +
+                "(KHTML, like Gecko) Chrome/20 " +
+                "Safari/537.31");
+        web.getSettings().setDefaultZoom(WebSettings.ZoomDensity.CLOSE);
+        web.loadUrl("http://ta.yrdsb.ca/yrdsb/");
+
 	}
 
 }
