c1f79d4f1696dadd2b74bdbd9fde45727d3af561
Priyesh <priyesh.96@hotmail.com>
2014-02-20 18:09:48 -0500

    Improve updater: - Check to see if the currently installed version is less than the highest version available on my server. If server has a greater version, download that apk - If currently installed version is equal to the highest version on server (user already has latest), display toast

--- src/com/example/teachassist/About.java
+++ src/com/example/teachassist/About.java
@@ -2,6 +2,8 @@ package com.example.teachassist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
@@ -10,10 +12,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class About extends Activity {
 
 	Button source, contact, download;
+	double latestVersion;
+	String installedVersion;
+	double installedVersionValue;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +50,40 @@ public class About extends Activity {
 		    }
 		});
 		
+		
+        try {
+            PackageInfo appInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            installedVersion = appInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            // Handle exception
+        }   
+        
+        //Latest version available on my server. Must update this value for each new release
+		latestVersion = 0.5;
+		
+		//Convert string value of installed version to double so that it can be compared with value of latest version		
+		installedVersionValue = Double.parseDouble(installedVersion); 
+		
 		download = (Button) findViewById(R.id.bDownload);
 		
 		download.setOnClickListener(new OnClickListener() {
 		    public void onClick(View v) {
-		        Intent downloadFromServer = new Intent();
-		        downloadFromServer.setAction(Intent.ACTION_VIEW);
-		        downloadFromServer.addCategory(Intent.CATEGORY_BROWSABLE);
-		        downloadFromServer.setData(Uri.parse("http://priyeshserver.tk/Files/TeachAssist"));
-		        startActivity(downloadFromServer);
+		    	
+		    	if (installedVersionValue<latestVersion) { //If latest version available on server is greater than installed version, download the latest
+		    		Intent downloadFromServer = new Intent();
+		    		downloadFromServer.setAction(Intent.ACTION_VIEW);
+		    		downloadFromServer.addCategory(Intent.CATEGORY_BROWSABLE);
+		    		downloadFromServer.setData(Uri.parse("http://priyeshserver.tk/Files/TeachAssist/TeachAssist-" + latestVersion + ".apk"));
+		    		startActivity(downloadFromServer);
+		    	}
+		    	else if (installedVersionValue==latestVersion) { //If user clicks the update button while they already have the latest, let them no what's up
+				    Toast.makeText(getApplicationContext(), "You are already running the latest version (" + installedVersionValue +")",
+				    Toast.LENGTH_LONG).show();	
+		    	}
 		    }
-		});
+		}); 
+		
+		
 		
 	}
 
