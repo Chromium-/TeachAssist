0ffa0b981efe54b090b30ef982f11120e2e90d2b
Priyesh <priyesh.96@hotmail.com>
2014-03-12 23:57:52 -0400

    Completely redesign about activity - Use Preference Screen instead, so all previous components and intents have been slightly modifed to work

--- src/com/example/teachassist/About.java
+++ src/com/example/teachassist/About.java
@@ -1,10 +1,12 @@
 package com.example.teachassist;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -14,13 +16,17 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class About extends Activity {
+public class About extends PreferenceActivity {
 
-	Button source, contact, download;
+	//Button source, contact, download;
 	String installedVersion; //version num taken from manifest. can only be declared as string
 	double latestVersion, installedVersionValue; //cast the string above into a double	
 	String latestOnServerString;
@@ -29,29 +35,32 @@ public class About extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);		
+		addPreferencesFromResource(R.xml.about_preference);	
 
-		source = (Button) findViewById(R.id.bSource);
-
-		source.setOnClickListener(new OnClickListener() { //open git repo of this app
-			public void onClick(View v) {
-				Intent downloadFromServer = new Intent();
-				downloadFromServer.setAction(Intent.ACTION_VIEW);
-				downloadFromServer.addCategory(Intent.CATEGORY_BROWSABLE);
-				downloadFromServer.setData(Uri.parse("https://github.com/Chromium-/TeachAssist"));
-				startActivity(downloadFromServer);
+		ActionBar ab = getActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+		
+		Preference source = (Preference) findPreference("source");
+		source.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				Intent git = new Intent();
+				git.setAction(Intent.ACTION_VIEW);
+				git.addCategory(Intent.CATEGORY_BROWSABLE);
+				git.setData(Uri.parse("https://github.com/Chromium-/TeachAssist"));
+				startActivity(git);
+				return true; 
 			}
 		});
 
-		contact = (Button) findViewById(R.id.bContact);
-
-		contact.setOnClickListener(new OnClickListener() { //open email intent
-			public void onClick(View v) {
-				Intent downloadFromServer = new Intent();
-				downloadFromServer.setAction(Intent.ACTION_VIEW);
-				downloadFromServer.addCategory(Intent.CATEGORY_BROWSABLE);
-				downloadFromServer.setData(Uri.parse("mailto:priyesh.96@hotmail.com"));
-				startActivity(downloadFromServer);
+		Preference contact = (Preference) findPreference("contact");
+		contact.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				Intent email = new Intent();
+				email.setAction(Intent.ACTION_VIEW);
+				email.addCategory(Intent.CATEGORY_BROWSABLE);
+				email.setData(Uri.parse("mailto:priyesh.96@hotmail.com"));
+				startActivity(email);
+				return true; 
 			}
 		});
 
@@ -65,9 +74,76 @@ public class About extends Activity {
 		//Convert string value of installed version to double so that it can be compared with value of latest version		
 		installedVersionValue = Double.parseDouble(installedVersion); 
 
-		download = (Button) findViewById(R.id.bDownload);
+		//download = (Button) findViewById(R.id.bDownload);
+		
+		Preference update = (Preference) findPreference("update");
+		update.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				final AsyncTask<Object,Object,String> task = new AsyncTask<Object,Object,String>() {
+					protected String doInBackground(Object... o) {
+						try {
+							URL site = new URL("http://70.49.210.232/Files/TeachAssist/latest.txt");
+							Scanner s = new Scanner(site.openStream());
+							return s.nextLine();
+						}
+						catch(IOException e2) {
+							throw new RuntimeException("Noooo crashhhsdhfhdfhsdhf", e2);
 
-		download.setOnClickListener(new OnClickListener() {
+						} 
+					}
+					
+					
+					protected void onPostExecute(String latestOnServerString) {
+
+						latestOnServerValue = Double.parseDouble(latestOnServerString);
+
+						if (installedVersionValue<latestOnServerValue) { 
+							//If latest version available on server is higher than installed version
+							AlertDialog.Builder builder = new AlertDialog.Builder(About.this);
+							builder.setMessage("Version " + latestOnServerValue + " was found on the server.\n\nWould you like to install it?")
+							.setTitle ("Update available")
+							.setCancelable(false)
+							.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int id) {
+									Intent downloadFromServer = new Intent();
+									downloadFromServer.setAction(Intent.ACTION_VIEW);
+									downloadFromServer.addCategory(Intent.CATEGORY_BROWSABLE);
+									downloadFromServer.setData(Uri.parse("http://70.49.210.232/Files/TeachAssist/TeachAssist-" + latestOnServerValue + ".apk"));
+									startActivity(downloadFromServer);
+								}
+							})
+							.setNegativeButton("No", new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int id) {
+									dialog.cancel();
+								}
+							});
+							AlertDialog updateAlert = builder.create();
+							updateAlert.show();					    				    		
+						}
+
+						else if (installedVersionValue==latestOnServerValue) { 
+						//If user clicks the update button while they already have the latest, let them know what's up
+							AlertDialog.Builder builder2 = new AlertDialog.Builder(About.this);
+							builder2.setMessage("You are already on the latest version:" + installedVersionValue)
+							.setTitle ("No update available")
+							.setCancelable(false)
+							.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog, int id) {
+									dialog.cancel();
+								}
+							});
+							AlertDialog noUpdateAlert = builder2.create();
+							noUpdateAlert.show();	
+							
+						}					    	
+					}		    		   
+				};
+				task.execute();	
+				return true; 
+			}
+		});
+		
+	/*	download.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 
 				final AsyncTask<Object,Object,String> task = new AsyncTask<Object,Object,String>() {
@@ -132,6 +208,6 @@ public class About extends Activity {
 				};
 				task.execute();			    		
 			}	    
-		}); 					
+		}); 		*/			
 	}
 }
