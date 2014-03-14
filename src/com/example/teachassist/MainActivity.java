8568721427ef57424c571b20bf046702eff19ab6
Priyesh <priyesh.96@hotmail.com>
2014-03-14 10:00:38 -0400

    Fix exit intent

--- src/com/example/teachassist/MainActivity.java
+++ src/com/example/teachassist/MainActivity.java
@@ -24,71 +24,71 @@ public class MainActivity extends Activity {
 
 	EditText username, password;
 	Button login;
-	
+
 	CheckBox rememberCheckBox;
 	SharedPreferences loginCredentials;
 	SharedPreferences.Editor loginCredentialsEditor;
 	Boolean remember;
-	    
+
 	public static String usernameString;
 	public static String passwordString;	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	    
+
 		// Look up the AdView as a resource and load a request.
-	    AdView adView = (AdView)this.findViewById(R.id.adView);
-	    AdRequest adRequest = new AdRequest.Builder()
-	    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
-	    .addTestDevice("2797F5D9304B6B3A15771A0519A4F687")  // HTC Desire
-	    .build();
-	    adView.loadAd(adRequest);		
-		
+		AdView adView = (AdView)this.findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder()
+		.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+		.addTestDevice("2797F5D9304B6B3A15771A0519A4F687")  // HTC Desire
+		.build();
+		adView.loadAd(adRequest);		
+
 		username = (EditText) findViewById(R.id.etUsername);
 		username.setInputType(InputType.TYPE_CLASS_TEXT);
-		
+
 		password = (EditText) findViewById(R.id.etPassword);
 		password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
 
-	    rememberCheckBox = (CheckBox)findViewById(R.id.cbRemember);
-	    loginCredentials = getSharedPreferences("loginPrefs", MODE_PRIVATE);
-	    loginCredentialsEditor = loginCredentials.edit();
+		rememberCheckBox = (CheckBox)findViewById(R.id.cbRemember);
+		loginCredentials = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+		loginCredentialsEditor = loginCredentials.edit();
 
-	    remember = loginCredentials.getBoolean("remember", false);
-	        if (remember == true) {
-	        	username.setText(loginCredentials.getString("username", ""));
-	        	password.setText(loginCredentials.getString("password", ""));
-	        	rememberCheckBox.setChecked(true);
-	        }	    
-	    
+		remember = loginCredentials.getBoolean("remember", false);
+		if (remember == true) {
+			username.setText(loginCredentials.getString("username", ""));
+			password.setText(loginCredentials.getString("password", ""));
+			rememberCheckBox.setChecked(true);
+		}	    
+
 		login = (Button) findViewById(R.id.bLogin);
-		
+
 		login.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v){
-				
+
 				usernameString = username.getText().toString();
 				passwordString = password.getText().toString();
 
-	            if (rememberCheckBox.isChecked()) {
-	            	loginCredentialsEditor.putBoolean("remember", true);
-	            	loginCredentialsEditor.putString("username", usernameString);
-	            	loginCredentialsEditor.putString("password", passwordString);
-	            	loginCredentialsEditor.commit();
-	            } 
-	            else {
-	            	loginCredentialsEditor.clear();
-	            	loginCredentialsEditor.commit();
-	            }
-	            
+				if (rememberCheckBox.isChecked()) {
+					loginCredentialsEditor.putBoolean("remember", true);
+					loginCredentialsEditor.putString("username", usernameString);
+					loginCredentialsEditor.putString("password", passwordString);
+					loginCredentialsEditor.commit();
+				} 
+				else {
+					loginCredentialsEditor.clear();
+					loginCredentialsEditor.commit();
+				}
+
 				if (usernameString.length()!=0 && passwordString.length()!=0) {
-				    Intent browser = new Intent(MainActivity.this, Browser.class);
-				    startActivity(browser);
+					Intent browser = new Intent(MainActivity.this, Browser.class);
+					startActivity(browser);
 				}
 				else {
-				    Toast.makeText(getApplicationContext(), "Please enter a username and password",
-				    Toast.LENGTH_SHORT).show();					
+					Toast.makeText(getApplicationContext(), "Please enter a username and password",
+							Toast.LENGTH_SHORT).show();					
 				}
 			}
 		});				
@@ -96,37 +96,40 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main, menu);
-	    return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem menuItem) {
 		if (menuItem.getItemId() == R.id.menu_about) { //about button in actionbar
-	         openAbout();
-	         return true;
-	    }
+			openAbout();
+			return true;
+		}
 		else if (menuItem.getItemId() == R.id.menu_exit) { //exit button in actionbar
 			exit();
 			return true;
 		}
-	    return false;
+		return false;
 	}   
-    
-    private void openAbout() { //open about section
-    	Intent about = new Intent(MainActivity.this, About.class);
+
+	private void openAbout() { //open about section
+		Intent about = new Intent(MainActivity.this, About.class);
 		startActivity(about);
-    }
-    
-    private void exit() { //exit app
-    	AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+	}
+
+	private void exit() { //exit app
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 		builder.setMessage("Are you sure you want to exit?")
 		.setTitle ("Confirm exit")
 		.setCancelable(false)
 		.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int id) {
-				System.exit(0);
+				Intent exit = new Intent(Intent.ACTION_MAIN);
+				exit.addCategory(Intent.CATEGORY_HOME);
+				exit.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(exit);
 			}
 		})
 		.setNegativeButton("No", new DialogInterface.OnClickListener() {
@@ -136,6 +139,6 @@ public class MainActivity extends Activity {
 		});
 		AlertDialog updateAlert = builder.create();
 		updateAlert.show();		
-    	
-    }
+
+	}
 }
